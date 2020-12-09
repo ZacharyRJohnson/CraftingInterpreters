cf2fbb6abb30b6b73bc83a64bc7a28e4ad685a6f
Zachary <zachjohnson1900@gmail.com>
2020-12-08 22:21:05 -0500

    Initial commit

---
+++ jlox/src/interpreter/TokenType.java
@@ -0,0 +1,25 @@
+package interpreter;
+
+public enum TokenType {
+    // Single char tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One to two char tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, OR,
+    CLASS, SUPER, THIS, FUN,
+    IF, ELSE, FOR, WHILE,
+    FALSE, TRUE,
+    PRINT, RETURN, VAR, NIL,
+
+    EOF
+}
